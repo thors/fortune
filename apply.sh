8c75a629d645fe70221863e941eb84d04e3833f5
Thorsten Moellers <thorsten.3.moellers@here.com>
2017-11-29 22:42:40 +0100

    Add option for private files, update gitignore to ingore these files

--- apply.sh
+++ apply.sh
@@ -4,6 +4,8 @@ declare preparation_folder=fortune_prepared
 declare target_folder=/usr/share/games/fortune 
 rm -rf "$preparation_folder"
 mkdir "$preparation_folder"
+
+
 find data -name "*.txt" | while read file
 do
 	declare textfile=$(echo $file | sed "s/\.txt$//" | sed "s/^data\///" )
@@ -11,5 +13,12 @@ do
 	strfile "$preparation_folder/$textfile" "$preparation_folder/${textfile}.dat"
 done
 
+find data_private -name "*.txt" | while read file
+do
+	declare textfile=$(echo $file | sed "s/\.txt$//" | sed "s/^data_private\///" )
+	cp $file "$preparation_folder/$textfile"
+	strfile "$preparation_folder/$textfile" "$preparation_folder/${textfile}.dat"
+done
+
 echo "preparation done... Next step will probably fail when you are not root"
-cp $preparation_folder $target_folder
+cp $preparation_folder/* $target_folder
